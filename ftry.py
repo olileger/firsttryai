c98b1fe7e537d380b8dfc093bf64f3603b968a95
Olivier <oliv.leger@gmail.com>
2025-04-09 14:01:13 +0200

    Reworking the ftry file with support for --prompt param

--- ftry.py
+++ ftry.py
@@ -3,45 +3,86 @@ import asyncio
 from autogen_agentchat.ui import Console
 
 
-async def main():
-    """
-    Entry point for the ftry | yai CLI.
-    Note for the dumb creator of this CLI => In dev mode:
-    - pip install e .       # install the package in editable mode => TODO after each change into setup.py
-    - pip uninstall ftry    # remove the package from the environment
-    """
+#
+# runTask
+#
+# This function runs the task using the Console UI.
+# Either for Team or Agent.
+#
+async def runTask(args, o):
+    if args.prompt:
+        task = args.prompt
+        print("Task passed along: ", task)
+    else:
+        task = input("Task: ")
+    await Console(o.run_stream(task=task))
 
+
+#
+# runPop
+#
+# This function runs the pop command.
+#
+async def runPop(args):
+    if args.team:
+        print("Creating team from file: ", args.team)
+        from src import TeamHelper
+        t = await TeamHelper.createTeam(args.team)
+        await runTask(args, t)
+    elif args.agent:
+        print("Creating agent from file: ", args.agent)
+        from src import AgentHelper
+        a = await AgentHelper.createAgent(args.agent)
+        await runTask(args, a)
+
+
+#
+# runKickflip
+#
+# This function runs the kickflip command.
+#
+def runKickflip():
+    print("Hey bro! Do a kickflip!")
+
+
+#
+# main
+#
+# This function is the entry point for the ftry CLI.
+#
+async def main():
     # Commands: definitions
     p = argparse.ArgumentParser(description="ftry CLI")
     subcmd = p.add_subparsers(dest="subcmd", required=True)
 
     # 'pop' command
     pop = subcmd.add_parser("pop", help="Pop up an agent or team")
+    pop.add_argument("-p", "--prompt", type=str, help="The prompt to use")
     popgroup = pop.add_mutually_exclusive_group(required=True)
     popgroup.add_argument("-t", "--team", type=str, help="Team description file")
     popgroup.add_argument("-a", "--agent", type=str, help="Agent description file")
 
     # 'kickflip' command
-    kickflip = subcmd.add_parser("kickflip", help="Ask for a kickflip")
+    subcmd.add_parser("kickflip", help="Ask for a kickflip")
 
     # Commands: function calling
     args = p.parse_args()
     if args.subcmd == "pop":
-        if args.team:
-            print("Creating team from file: ", args.team)
-            from src import TeamHelper
-            t = await TeamHelper.createTeam(args.team)
-            task = input("Task: ")
-            await Console(t.run_stream(task=task))
-        elif args.agent:
-            print("Creating agent from file: ", args.agent)
-            from src import AgentHelper
-            a = await AgentHelper.createAgent(args.agent)
-            task = input("Task: ")
-            await Console(a.run_stream(task=task))
+        await runPop(args)
     elif args.subcmd == "kickflip":
-        print("Hey bro! Do a kickflip!")
+        runKickflip()
 
 
-if __name__ == "__main__":
+#
+# main_sync
+#
+# This function is a wrapper for the main function to run it synchronously
+# either from the command line or from another script.
+#
+def main_sync():
     asyncio.run(main())
+
+
+# Entry point for the script.
+if __name__ == "__main__":
+    main_sync()
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+"""
+Note for the dumb creator of this CLI => In dev mode:
+- pip install e .       # install the package in editable mode => TODO after each change into setup.py
+- pip uninstall ftry    # remove the package from the environment
+"""
 setup(
     name="ftry",
     version="0.1",
     py_modules=["ftry"],
     entry_points={
         "console_scripts": [
-            "ftry=ftry:main",
-            "yai=ftry:main",
+            "ftry=ftry:main_sync",
+            "yai=ftry:main_sync",
         ],
     },
 )
