e8c029fcf2205cd42337b55e2960fcb5c5a82a5d
Olivier <oliv.leger@gmail.com>
2026-03-10 15:15:30 +0100

    Creating a commit message for the changes made to the Agent and AgentHelper modules.

--- src/Agent.py
+++ src/Agent.py
@@ -1,20 +1,23 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+
 class Agent:
     """Provide abstraction for an Agent for an easy library switching."""
     
-    def __init__(self, name: str, instruction: str, model: str):
-        self.name = name
-        self.instruction = instruction
-        self.model = model
-        # TODO: create the underlying agent on OpenAI SDK and move the property reading to the underlying agent.
+    def __init__(self, name: str, instruction: str, model_client: OpenAIChatCompletionClient):
+        self._name = name
+        self._instruction = instruction
+        self._agent = AssistantAgent(
+            name=self._name,
+            system_message=self._instruction,
+            model_client=model_client
+        )
     
     @property
     def name(self) -> str:
-        return self.name
+        return self._name
     
     @property
     def instruction(self) -> str:
-        return self.instruction
-    
-    @property
-    def model(self) -> str:
-        return self.model
+        return self._instruction
--- src/AgentHelper.py
+++ src/AgentHelper.py
@@ -1,11 +1,12 @@
-from src import FileHelper
+from src import FileHelper, ModelHelper
 from src.Agent import Agent
 
 
 async def createAgent(filePath: str) -> Agent:
     file = FileHelper.readYamlFile(filePath)
     try:
-        a = Agent(name=file["name"], instruction=file["prompt"], model=file["model"])
-        return a
+        model = FileHelper.interpretYamlModelObject(file["model"])
+        model_client = ModelHelper.createModel(model["name"], model["provider"], model["api-key"])
+        return Agent(name=file["name"], instruction=file["prompt"], model_client=model_client)
     except KeyError as e:
         raise Exception(f"YAML file doesn't contains key: {e}")
